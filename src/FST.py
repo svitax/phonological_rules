d1c9dabdda61c908a0235a795c01cc191324aa4a
Igor Barale <svitaxiom@gmail.com>
2021-03-30 21:30:59 -0400

    added Phoneme class constructor

--- src/FST.py
+++ src/FST.py
@@ -2,3 +2,23 @@ import json
 
 feature_matrices = open('phoneme.json')
 fm = json.load(feature_matrices)
+
+
+class Phoneme:
+    def __init__(self, symbol, name=None, features=None):
+        if symbol == '#':
+            self.symbol = '#'
+            self.name = 'word boundary'
+            self.features = {'boundary': True}
+        else:
+            self.symbol = symbol
+
+            if name:
+                self.name = name
+            else:
+                self.name = fm[self.symbol]['name']
+
+            if features:
+                self.features = features
+            else:
+                self.features = fm[self.symbol]['features']
